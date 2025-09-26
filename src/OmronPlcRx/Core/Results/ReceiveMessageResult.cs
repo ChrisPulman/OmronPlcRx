0778c6a00218a0b7e40eb42c2e67dc9e568c047e
Chris Pulman <chris.pulman@yahoo.com>
2025-09-26 06:36:38 +0100

    Add result structs and custom exceptions for Omron PLC
    Introduces ProcessRequestResult, ReceiveMessageResult, and SendMessageResult structs to encapsulate communication results. Adds FINSException and OmronException classes for improved error handling in Omron PLC communication.

---
+++ src/OmronPlcRx/Core/Results/ReceiveMessageResult.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Chris Pulman. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace OmronPlcRx.Core.Results;
+
+internal record struct ReceiveMessageResult
+{
+    internal Memory<byte> Message;
+    internal int Bytes;
+    internal int Packets;
+}
---
+++ src/OmronPlcRx/Core/Results/SendMessageResult.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Chris Pulman. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace OmronPlcRx.Core.Results;
+
+internal record struct SendMessageResult
+{
+    internal int Bytes;
+    internal int Packets;
+}
