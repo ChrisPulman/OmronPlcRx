3b617ce038410132edbf4f862fd7e4c757762dab
Chris Pulman <chris.pulman@yahoo.com>
2025-09-28 16:29:49 +0100

    Refactor tag handling to use IPlcTag interface
    Introduces the IPlcTag interface and updates PlcTag<T> to implement it, enabling more type-safe and extensible tag management. Refactors IOmronPlcRx and OmronPlcRx to use IPlcTag for tag observation and notification, improving code clarity and future extensibility. Also adds conditional package references for netstandard2.0 in the project file.

--- src/OmronPlcRx/IOmronPlcRx.cs
+++ src/OmronPlcRx/IOmronPlcRx.cs
@@ -3,64 +3,64 @@
 
 using System;
 using System.Reactive.Disposables;
+using OmronPlcRx.Tags;
 
-namespace OmronPlcRx
+namespace OmronPlcRx;
+
+/// <summary>
+/// IOmronPlcRx.
+/// </summary>
+public interface IOmronPlcRx : ICancelable
 {
     /// <summary>
-    /// IOmronPlcRx.
+    /// Gets the observe all.
     /// </summary>
-    public interface IOmronPlcRx : ICancelable
-    {
-        /// <summary>
-        /// Gets the observe all.
-        /// </summary>
-        /// <value>
-        /// The observe all.
-        /// </value>
-        IObservable<object?> ObserveAll { get; }
+    /// <value>
+    /// The observe all.
+    /// </value>
+    IObservable<IPlcTag?> ObserveAll { get; }
 
-        /// <summary>
-        /// Gets the errors.
-        /// </summary>
-        /// <value>
-        /// The errors.
-        /// </value>
-        IObservable<OmronPLCException?> Errors { get; }
+    /// <summary>
+    /// Gets the errors.
+    /// </summary>
+    /// <value>
+    /// The errors.
+    /// </value>
+    IObservable<OmronPLCException?> Errors { get; }
 
-        /// <summary>
-        /// Adds the update tag item.
-        /// </summary>
-        /// <typeparam name="T">The type to observe.</typeparam>
-        /// <param name="variable">The PLC variable.</param>
-        /// <param name="tagName">Name of the tag.</param>
-        void AddUpdateTagItem<T>(string variable, string tagName);
+    /// <summary>
+    /// Adds the update tag item.
+    /// </summary>
+    /// <typeparam name="T">The type to observe.</typeparam>
+    /// <param name="variable">The PLC variable.</param>
+    /// <param name="tagName">Name of the tag.</param>
+    void AddUpdateTagItem<T>(string variable, string tagName);
 
-        /// <summary>
-        /// Observes the specified variable.
-        /// </summary>
-        /// <typeparam name="T">The PLC type.</typeparam>
-        /// <param name="tagName">The Tag Name.</param>
-        /// <returns>
-        /// An observable sequence of values of type T.
-        /// </returns>
-        IObservable<T?> Observe<T>(string? tagName);
+    /// <summary>
+    /// Observes the specified variable.
+    /// </summary>
+    /// <typeparam name="T">The PLC type.</typeparam>
+    /// <param name="tagName">The Tag Name.</param>
+    /// <returns>
+    /// An observable sequence of values of type T.
+    /// </returns>
+    IObservable<T?> Observe<T>(string? tagName);
 
-        /// <summary>
-        /// Reads the specified variable.
-        /// </summary>
-        /// <typeparam name="T">The PLC type.</typeparam>
-        /// <param name="tagName">The Tag Name.</param>
-        /// <returns>
-        /// A value of T.
-        /// </returns>
-        T? Value<T>(string? tagName);
+    /// <summary>
+    /// Reads the specified variable.
+    /// </summary>
+    /// <typeparam name="T">The PLC type.</typeparam>
+    /// <param name="tagName">The Tag Name.</param>
+    /// <returns>
+    /// A value of T.
+    /// </returns>
+    T? Value<T>(string? tagName);
 
-        /// <summary>
-        /// Writes the specified variable value.
-        /// </summary>
-        /// <typeparam name="T">The PLC type.</typeparam>
-        /// <param name="tagName">The Tag Name.</param>
-        /// <param name="value">The value.</param>
-        void Value<T>(string? tagName, T? value);
-    }
+    /// <summary>
+    /// Writes the specified variable value.
+    /// </summary>
+    /// <typeparam name="T">The PLC type.</typeparam>
+    /// <param name="tagName">The Tag Name.</param>
+    /// <param name="value">The value.</param>
+    void Value<T>(string? tagName, T? value);
 }
---
+++ src/OmronPlcRx/Tags/IPlcTag.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Chris Pulman. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace OmronPlcRx.Tags
+{
+    /// <summary>
+    /// IPlcTag.
+    /// </summary>
+    public interface IPlcTag
+    {
+        /// <summary>
+        /// Gets the address.
+        /// </summary>
+        /// <value>
+        /// The address.
+        /// </value>
+        string Address { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is bit address.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is bit address; otherwise, <c>false</c>.
+        /// </value>
+        Type TagType { get; }
+
+        /// <summary>
+        /// Gets the name of the tag.
+        /// </summary>
+        /// <value>
+        /// The name of the tag.
+        /// </value>
+        string TagName { get; }
+
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        object? Value { get; }
+    }
+}
